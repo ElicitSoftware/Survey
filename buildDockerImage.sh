e56823f642dededd6aa6216ae8dee8b7cb221153
Matthew Demerath <m.demerath@elicitsoftware.com>
2025-06-10 16:34:31 -0400

    Removed commented line

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -2,4 +2,5 @@ echo "Set java to 21"
 jenv local 21.0.2
 echo "build survey"
 
-./mvnw clean package -Dmaven.test.skip=true -Dquarkus.profile=docker
+./mvnw clean package -Dmaven.test.skip=true -Dquarkus.profile=docker -Pproduction
+# ./mvnw clean package -Dmaven.test.skip=true -Dnative -Dquarkus.native.container-build=true -Dquarkus.container-image.build=true -Dquarkus.profile=docker
