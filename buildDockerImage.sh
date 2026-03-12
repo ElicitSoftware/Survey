8481391ed321cd58b67b69969a4b44e18887a715
Matthew Demerath <mdemerat@med.umich.edu>
2026-03-12 10:22:03 -0400

    Upgrade to Vaadin 25.0.7

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -2,4 +2,4 @@ echo "Set java to 21"
 jenv local 21
 echo "build survey"
 
-./mvnw clean package -Dmaven.test.skip=true -Dquarkus.profile=docker -Pproduction
+./mvnw clean package -Dmaven.test.skip=true -Dquarkus.profile=docker
